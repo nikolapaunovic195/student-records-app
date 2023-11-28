17e04c1928e37b0f8a722ad77b23098897664761
nikolapaunovic195 <npaunovic195@gmail.com>
2023-11-27 22:22:41 -0800

    Create ActivityLevel.kt

---
+++ src/main/kotlin/ActivityLevel.kt
@@ -0,0 +1,15 @@
+/**
+ * Enum class for activity levels
+ * @property ABSENT student is not present
+ * @property INATTENTIVE student is present and actively not paying attention
+ * @property PASSIVE student is present and passively paying attention
+ * @property ACTIVE student is present and participating in some way
+ * @property VERY_ACTIVE student is present and participating in a very active way
+ */
+enum class ActivityLevel {
+    ABSENT,
+    INATTENTIVE,
+    PASSIVE,
+    ACTIVE,
+    VERY_ACTIVE
+}
